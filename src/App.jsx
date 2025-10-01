1ff7c7b2088ee6ad21fa578810cd8f6dcf11a3a8
Nayeli <naymamanicaqui@crackthecode.la>
2025-10-01 10:04:32 -0500

    Clase 50

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CustomComponents from './assets/Components/CustomComponents'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className= "text-4xl , font-bold, text center my-10">Hola Taiwandal CSS  </h1>
+    <CustomComponents />
+    </div>
+
+  );
+};
+
+export default App;
